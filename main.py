632b5f1e634a0c1573d00bc4337ea7a69a7217a0
Kateryna Kuzminska <kateryna.kuzminska@student.kyh.se>
2021-10-03 21:36:24 +0200

    Iteration2

--- main.py
+++ main.py
@@ -1,49 +1,54 @@
 import random
+import time
+import json
 
 # Questions and answers
-questionsAndAnswers = [
-      ("Vilken funktion används för att skriva ut saker på skärmen?", "print"),
-      ("Vad heter nyckelordet för att göra en loop i Python?", "for"),
-      ("Hur tar man fram längden på listan i variabeln \"fruits\"?", "list(fruits)"),
-      ("fråga4", "svar4"),
-      ("fråga5", "svar5"),
-      ("fråga6", "svar6"),
-      ("fråga7", "svar7"),
-      ("fråga8", "svar8"),
-      ("fråga9", "svar9"),
-      ("fråga10", "svar10"),
-      ("fråga11", "svar11"),
-      ("fråga12", "svar12"),
-      ("fråga13", "svar13"),
-      ("fråga14", "svar14"),
-      ("fråga15", "svar15"),
-      ("fråga16", "svar16"),
-      ("fråga17", "svar17"),
-      ("fråga18", "svar18"),
-      ("fråga19", "svar19"),
-      ("fråga20", "svar20")
-      ]
+questionsAndAnswers = json.load(open('data.json', 'r', encoding='utf-8'))
 
 askedQuestions = list()  # list of already asked questions to avoid duplicate questions
 questionsCount = len(questionsAndAnswers) # amount of questions and answers
 
 # main program
-print("Välkommen till quizen. Vi ska ställa 5 frågor om Python.\nNu kör vi!\n")
+print("Välkommen till quizen. Vi ska ställa 10 frågor om Python.\n"
+        "Du får 5 poäng om du svarar korrekt inom 10 sekunder\n"
+        "Du får 4 poäng om du svarar korrekt inom 20 sekunder\n"
+        "Du får 3 poäng om du svarar korrekt inom 30 sekunder\n"
+        "Du får 2 poäng om du svarar korrekt inom 40 sekunder\n"
+        "Du får 1 poäng om du svarar korrekt efter 40 sekunder\n"
+        "Nu kör vi!\n")
 
 #pick up question from qa which is not already in askedQuestions
 index = int
+totalPoint = 0
+totalCorrectAnswer = 0
 
-for x in range(0, 5):
-      while True:
-            index = random.randint(0, questionsCount - 1)
-            if index not in askedQuestions:
-                  break
-      print(f"Question #{x+1}: {questionsAndAnswers[index][0]}")
-      askedQuestions.append(index)
-      answer = input("Ditt svar: ").strip()
-      if answer == questionsAndAnswers[index][1]:
-            print("Rätt!")
-      else:
-            print("Fel!")
+for x in range(0, 10):
+    while True:
+        index = random.randint(0, questionsCount - 1)
+        if index not in askedQuestions:
+            break
+    print(f"Question #{x+1}: {questionsAndAnswers[index]['question']}")
+    askedQuestions.append(index)
+    startTime = int(time.time())
+    answer = input("Ditt svar: ").strip()
+    finishTime = int(time.time())
+    answerTime = finishTime - startTime
+    if answer == questionsAndAnswers[index]['answer']:
+        currentPoint = int
+        totalCorrectAnswer += 1
+        if answerTime <= 10:
+            currentPoint = 5
+        elif answerTime <= 20:
+            currentPoint = 4
+        elif answerTime <= 30:
+            currentPoint = 3
+        elif answerTime <= 40:
+            currentPoint = 2
+        else:
+            currentPoint = 1
+        totalPoint += currentPoint
+        print(f"Rätt! Du svarade på {answerTime} sekunder och får {currentPoint} poäng!\nDu har {totalPoint} poäng totalt!")
+    else:
+        print(f"Fel! Du får 0 poäng.\nDu har {totalPoint} poäng totalt!")
 
-print("done")
+print(f"\nGrattis!\nDu har svarat korrekt på {totalCorrectAnswer} frågor! Du har fått {totalPoint} poäng!")
