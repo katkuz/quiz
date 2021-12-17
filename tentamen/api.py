2328168b4a59ee36ca060bfcedb76dc371eaa248
Kateryna Kuzminska <kateryna.kuzminska@student.kyh.se>
2021-12-17 11:50:42 +0100

    Nya kommentarer. Jag är färdig med provet

--- tentamen/api.py
+++ tentamen/api.py
@@ -2,5 +2,10 @@ import requests
 # Dokumentation, hjälp samt verktyg för att testa apiet fins här: https://app.swaggerhub.com/apis/NobelMedia/NobelMasterData/2.1
 
 
-def get_nobel_prizes(c):
-    return requests.get("http://api.nobelprize.org/2.1/nobelPrizes", params=c).json()
+def get_nobel_prizes(parameters):
+    """Get list of nobel prizes for selected parameters
+
+        parameters
+            argument to be send to api to get desires info about prizes
+        """
+    return requests.get("http://api.nobelprize.org/2.1/nobelPrizes", params=parameters).json()
--- tentamen/nobel_prize.py
+++ tentamen/nobel_prize.py
@@ -8,64 +8,89 @@ from tentamen.print_strings import START_MESSAGE, WRONG_YEAR_MESSAGE, QUITING_ME
 def main():
     print(START_MESSAGE)
     while True:
-        thy_command = input(">").strip()
-        check_for_quit(thy_command)
-        if re.match(r"[0-9]{4}$", thy_command):
-            if get_year(thy_command) == -1:
+        user_input_sting = input(">").strip()
+        check_for_quit(user_input_sting)
+        if re.match(r"[0-9]{4}$", user_input_sting):
+            if get_year(user_input_sting) == -1:
                 continue
-            print_nobel_prizes(get_nobel_prizes({"nobelPrizeYear": get_year(thy_command)}))
+            print_nobel_prizes(get_nobel_prizes({"nobelPrizeYear": get_year(user_input_sting)}))
         else:
             try:
-                a, b = thy_command.split()
-                if get_year(a) == -1:
+                year, category = user_input_sting.split()
+                if get_year(year) == -1:
                     continue
-                if b not in cat.keys():
+                if category not in cat.keys():
                     print(WRONG_CATEGORY_MESSAGE)
                     continue
-                print_nobel_prizes(get_nobel_prizes({"nobelPrizeYear": int(a), "nobelPrizeCategory": cat[b]}))
+                print_nobel_prizes(get_nobel_prizes({"nobelPrizeYear": int(year), "nobelPrizeCategory": cat[category]}))
             except ValueError:
                 print(WRONG_INPUT_MESSAGE)
             continue
 
 
-def check_for_quit(thy_command):
-    if thy_command == 'Q' or thy_command == 'q':
+def check_for_quit(user_input_sting):
+    """Check input from user and quit program if user want it
+
+        user_input_sting
+            input string from user
+        """
+    if user_input_sting == 'Q' or user_input_sting == 'q':
         print(QUITING_MESSAGE)
         quit()
 
 
 def print_nobel_prizes(res):
-    for p in res["nobelPrizes"]:
-        money = p["prizeAmount"]
-        today_money = p["prizeAmountAdjusted"]
-        print(f"{p['categoryFullName']['se']} prissumma {money} SEK")
+    """Print list of nobel prizes from response
+
+        res
+            response as json object with info about prizes and laureates for selected parameters
+        """
+    for prize in res["nobelPrizes"]:
+        prize_money = prize["prizeAmount"]
+        prize_todays_money = prize["prizeAmountAdjusted"]
+        print(f"{prize['categoryFullName']['se']} prissumma {prize_money} SEK")
         print("Laureates:")
-        for m in p["laureates"]:
-            print_laureate(m, money, today_money, 0)
+        for laureate in prize["laureates"]:
+            print_laureate(laureate, prize_money, prize_todays_money)
         print("")
 
 
-def print_laureate(m, money, today_money, result):
+def print_laureate(laureate, prize_money, prize_todays_money):
+    """Print one laureate on screen from json object.
+
+        laureate
+            json object with info about a laureate
+        prize_money
+            total prize historical
+        prize_todays_money
+            total prize with inflation to current date
+        """
+    portion_number = 0
     try:
-        andel = m['portion']
-        if '/' in andel:
-            num, den = andel.split('/')
-            result = float(num) / float(den)
+        portion_string = laureate['portion']
+        if '/' in portion_string:
+            num, den = portion_string.split('/')
+            portion_number = float(num) / float(den)
         else:
-            result = int(andel)
+            portion_number = int(portion_string)
     except ValueError:
         pass
-    if 'knownName' in m.keys():
-        print(f"{m['knownName']['en']}: ", end='')
-    if 'orgName' in m.keys():
-        print(f"{m['orgName']['en']}: ", end='')
-    if result != 0:
-        print(f"{int(result * int(money))} / {int(result * int(today_money))} SEK", end='')
-    print(f" {m['motivation']['en']}")
+    if 'knownName' in laureate.keys():
+        print(f"{laureate['knownName']['en']}: ", end='')
+    if 'orgName' in laureate.keys():
+        print(f"{laureate['orgName']['en']}: ", end='')
+    if portion_number != 0:
+        print(f"{int(portion_number * int(prize_money))} / {int(portion_number * int(prize_todays_money))} SEK", end='')
+    print(f" {laureate['motivation']['en']}")
 
 
-def get_year(a):
-    match = re.match(r"[0-9]{4}$", a)
+def get_year(year_string):
+    """Try to get year from string return year as int. Return -1 if string is not a year between 1901 and current year.
+
+        year_string
+            string to parse
+        """
+    match = re.match(r"[0-9]{4}$", year_string)
     if not match:
         print(WRONG_YEAR_MESSAGE)
         return -1
